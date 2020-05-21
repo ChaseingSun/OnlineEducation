d28a41cd6aeb0034d88a1987e04d2f50e960febd
ChaseingSun <1825289506@qq.com>
2020-05-21 09:45:05 +0800

    集成pagehelper

--- src/main/java/com/lukas/onlineeducation/controller/VideoController.java
+++ src/main/java/com/lukas/onlineeducation/controller/VideoController.java
@@ -1,9 +1,15 @@
 package com.lukas.onlineeducation.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.lukas.onlineeducation.domain.Video;
 import com.lukas.onlineeducation.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * video接口
@@ -26,7 +32,16 @@ public class VideoController {
     @GetMapping("page")
     public Object pageVideo(@RequestParam(value = "page", defaultValue = "1") int page,
                             @RequestParam(value = "size", defaultValue = "10") int size){
-        return videoService.findAll();
+
+        PageHelper.startPage(page, size);
+        List<Video> list = videoService.findAll();
+        PageInfo<Video> pageInfo = new PageInfo<>(list);
+        Map<String,Object> data = new HashMap<>();
+        data.put("total_size",pageInfo.getTotal());
+        data.put("total_page",pageInfo.getPages());
+        data.put("current_page",page);
+        data.put("data",pageInfo.getList());
+        return data;
     }
 
     /**
